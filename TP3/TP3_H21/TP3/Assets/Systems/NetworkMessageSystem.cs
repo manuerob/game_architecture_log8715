3f311c48284d22ac46723ed7830f4a9ed4ac46d5
Emmanuelle Roberge <emmanuelle.roberge@polymtl.ca>
2021-03-24 16:05:00 -0400

    ER : Added TP3

---
+++ TP3/TP3_H21/TP3/Assets/Systems/NetworkMessageSystem.cs
@@ -0,0 +1,37 @@
+﻿public class NetworkMessageSystem : ISystem
+{
+    public string Name
+    {
+        get
+        {
+            return GetType().Name;
+        }
+    }
+
+    // In charge of sending all messages pending sending
+    public void UpdateSystem()
+    {
+        bool messagingInfoFound = ComponentsManager.Instance.TryGetComponent(new EntityComponent(0), out MessagingInfo messagingInfo);
+
+        if (!messagingInfoFound)
+        {
+            messagingInfo = new MessagingInfo() { currentMessageId = 0 };
+        }
+
+        if (ECSManager.Instance.NetworkManager.isServer)
+        {
+            ComponentsManager.Instance.ForEach<ReplicationMessage>((entityID, msg) =>
+            {
+                msg.messageID = messagingInfo.currentMessageId++;
+                ECSManager.Instance.NetworkManager.SendReplicationMessage(msg);
+            });
+        }
+
+        if (ECSManager.Instance.NetworkManager.isClient)
+        {
+            // TODO
+        }
+
+        ComponentsManager.Instance.SetComponent<MessagingInfo>(new EntityComponent(0), messagingInfo);
+    }
+}
